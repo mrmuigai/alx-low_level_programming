f73b362288fef610ccbda9f40f87a2c8b1000dff
mrmuigai <eugenemuigai254@gmail.com>
2023-02-21 18:16:02 -0800

    m

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/**
- * _isalpha - checks if char is lowercase
+/*
+ * _isalpha - checks for char alphabet
  * @c: is the char to be checked
  *
  * Return: 1 if char is lowercase, otherwise 0.
